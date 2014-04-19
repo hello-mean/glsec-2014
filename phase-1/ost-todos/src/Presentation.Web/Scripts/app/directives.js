b58543cd76687376b18d024a0003ec9ea041ae46
Jason Sich <jasich@gmail.com>
2014-04-19 15:16:20 -0400

    added todos phase1

---
+++ phase-1/ost-todos/src/Presentation.Web/Scripts/app/directives.js
@@ -0,0 +1,59 @@
+﻿angular.module('todos.directives', []).
+    directive('deleteList', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, elem, attrs, ctrl) {
+                elem.on('click', function() {
+                    if (confirm('Are you sure?')) {
+                        scope.list.$delete(function() {
+                            scope.$emit('list:deleted');
+                        });
+                    }
+                });
+            }
+        };
+    }).
+    directive('showAddList', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, elem, attrs) {
+                var modal = angular.element('#addListModal');
+                modal.on('shown', function() {
+                    $(this).find('input').focus();
+                });
+                elem.on('click', function(e) {
+                    modal.modal('show');
+                });
+                elem.scope().$on('list:added', function() {
+                    modal.modal('hide');
+                    modal.find('input').val('');
+                });
+            }
+        };
+    }).
+    directive('giveFocus', function() {
+        return {
+            restrict: 'A',
+            link: function(scope, elem, attrs) {
+                scope.$watch('list.Todos.length', function(newVal, oldVal) {
+                    if (newVal > oldVal)
+                        elem.focus();
+                });
+                elem.focus();
+            }
+        };
+    }).
+    directive('brandLink', function() {
+        return {
+            restrict: 'A',
+            link: function (scope, elem, attrs) {
+                scope.$watch('isLoggedIn()', function(newVal, oldVal) {
+                    if (newVal) {
+                        elem.attr('href', '/#lists');
+                    } else {
+                        elem.attr('href', '/#login');
+                    } 
+                });
+            }
+        };
+    });
---
+++ phase-1/ost-todos/src/Presentation.Web/Scripts/app/services.js
@@ -0,0 +1,12 @@
+﻿angular.module("todos.services", ['ngResource']).
+    factory("TodoList", ['$resource', function($resource) {
+        return $resource('/api/todolists/:id', { id: '@Id' });
+    }]).
+    factory("ListTodo", ['$resource', function($resource) {
+        return $resource('/api/todolists/:listId/todos');
+    }]).
+    factory("Todo", ['$resource', function($resource) {
+        return $resource('/api/todos/:id', {id:'@Id'}, {
+            update: {method:'PUT'}
+        });
+    }]);
