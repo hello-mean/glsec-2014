7c2d9116ce077758873c2ca85eede505579b1ca8
Nic Jansma <nic@nicj.net>
2014-04-20 23:10:17 -0400

    More CORS fixes

--- phase-1/node-server/http-server.js
+++ phase-1/node-server/http-server.js
@@ -24,6 +24,8 @@ var publicDir = webDir + 'public/';
  */
 function allowCrossDomain(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
 
     next();
 }
