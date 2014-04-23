71d9c4b95d2285683c34724ad179b53c3cca6371
Nic Jansma <nic@nicj.net>
2014-04-22 20:18:26 -0400

    Phase 2 beginnings

---
+++ phase-2/ost-todos/src/Presentation.Web/Controllers/TodosController.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Http;
+using Core.Domain.Model;
+using Core.Domain.Model.Todos;
+using Presentation.Web.Models.Display;
+using Presentation.Web.Models.Input;
+
+namespace Presentation.Web.Controllers
+{
+    public class TodosController : ControllerBase
+    {
+        private IRepository<Todo> _repo;
+
+        public TodosController(IRepository<Todo> repo)
+        {
+            _repo = repo;
+        }
+
+        [System.Web.Http.Authorize]
+        public HttpResponseMessage Put(long Id, TodoInput todoInput)
+        {
+            var todo = _repo.Get(Id);
+            todo.Completed = todoInput.Completed;
+            _repo.Store(todo);
+            return Request.CreateResponse(HttpStatusCode.OK, new TodoDisplay() {Id = Id, Title = todo.Title, Completed = todo.Completed});
+        }
+
+        [System.Web.Http.Authorize]
+        [System.Web.Http.HttpDelete]
+        public HttpResponseMessage Delete(long Id)
+        {
+            var todo = _repo.Load(Id);
+            _repo.Delete(todo);
+            return Request.CreateResponse(HttpStatusCode.NoContent);
+        }
+    }
+}
---
+++ phase-2/ost-todos/src/Test.Unit/Core/Domain/Model/Users/UserTest.cs
@@ -0,0 +1,45 @@
+﻿using Core.Domain.Model.TodoLists;
+using Core.Domain.Model.Users;
+using DevOne.Security.Cryptography.BCrypt;
+using NUnit.Framework;
+
+namespace Test.Unit.Core.Domain.Model.Users
+{
+    [TestFixture]
+    public class UserTest
+    {
+        [Test]
+        public void HashPassword_should_hash_unhashed_password()
+        {
+            var user = new User() {Password = "password"};
+            user.HashPassword();
+            Assert.IsTrue(BCryptHelper.CheckPassword("password", user.Password));
+        }
+
+        [Test]
+        public void IsAuthenticated_should_return_true_for_matching_unhashed_password()
+        {
+            var user = new User() {Password = "password"};
+            user.HashPassword();
+            Assert.True(user.IsAuthenticated("password"));
+        }
+
+        [Test]
+        public void IsAuthenticated_should_return_fals_for_non_matching_unhashed_password()
+        {
+            var user = new User() { Password = "password" };
+            user.HashPassword();
+            Assert.False(user.IsAuthenticated("false"));
+        }
+
+        [Test]
+        public void AddList_returns_list()
+        {
+            var list = new TodoList();
+            list.Name = "My First List";
+            var user = new User() {Email = "e@c.com", Name = "brian", Password = "pass"};
+            var copied = user.AddList(list);
+            Assert.AreSame(list, copied);
+        }
+    }
+}
